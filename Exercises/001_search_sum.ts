e35bab14d1da8ff4a52428baec479cac1215db3a
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-07-29 21:43:43 +0200

    Done 001 exercise.

---
+++ Exercises/001_search_sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+  const result = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (result.has(nums[i])) {
+      return [result.get(nums[i]), i];
+    }
+    result.set(target - nums[i], i);
+  }
+}
+
+export default twoSum;
