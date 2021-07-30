00a3607c3ecabb858ddbac4f1be4166cbaa83301
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-07-30 18:28:02 +0200

    Pass 'au' test.

--- Exercises/003_longest_substring_without_repeating_characters.ts
+++ Exercises/003_longest_substring_without_repeating_characters.ts
@@ -3,18 +3,24 @@ function lengthOfLongestSubstring(s: string): number {
 
   if (s.length < 2) return s.length;
 
-  s.split('').reduce((acc, letter) => {
+  s.split('').reduce((acc, letter, index, array) => {
     if (acc.includes(letter)) {
-      if (acc.length > result.length) {
+      if (acc.length >= result.length) {
         result = acc;
       }
       return letter;
     } else {
-      return (acc += letter);
+      if (index === array.length - 1) {
+        result = acc += letter;
+      } else {
+        return (acc += letter);
+      }
     }
   }, '');
 
   return result.length;
 }
+
+lengthOfLongestSubstring('au');
 
 export default lengthOfLongestSubstring;
