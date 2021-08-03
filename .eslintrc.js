46d0a01d884eaaaf544f7d5b76a7942b6d1b8e26
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-03 23:18:51 +0200

    Added prettier lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
