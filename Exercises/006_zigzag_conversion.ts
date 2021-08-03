6618ae3f2d58137a508cbe99b7741dc241a1e206
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-03 11:33:51 +0200

    LC 006. Passed all test. Refactor.

--- Exercises/006_zigzag_conversion.ts
+++ Exercises/006_zigzag_conversion.ts
@@ -1,21 +1,23 @@
 export default function convert(s: string, numRows: number): string {
   let isAsc = true;
   let row = 0;
-  const result = Array(numRows).fill([]);
 
-  s.split('').forEach((l) => {
-    result[row] = [...result[row], l];
-
-    if (numRows === 1) return;
-
-    if (isAsc) {
-      row += 1;
-      if (row === numRows - 1) isAsc = false;
-    } else {
-      row -= 1;
-      if (row === 0) isAsc = true;
-    }
-  }, []);
-
-  return result.flat().join('');
+  return s
+    .split('')
+    .reduce((acc, l) => {
+      acc[row] = [...(acc[row] ?? []), l];
+      if (numRows === 1) return acc;
+      if (isAsc) {
+        row += 1;
+        if (row === numRows - 1) isAsc = false;
+      } else {
+        row -= 1;
+        if (row === 0) isAsc = true;
+      }
+      return acc;
+    }, [])
+    .flat()
+    .join('');
 }
+
+console.log(convert('AB', 1));
