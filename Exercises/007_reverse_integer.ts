95f33d1a264145e8843fd638538fa6501efc1168
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-03 23:31:24 +0200

    LC 007. Passed all tests.

--- Exercises/007_reverse_integer.ts
+++ Exercises/007_reverse_integer.ts
@@ -1,7 +1,11 @@
 export default function reverse(x: number): number {
-  return +Math.abs(x)
+  const result = +Math.abs(x)
     .toString()
     .split('')
     .reverse()
     .reduce((acc, n) => acc + n, x >= 0 ? '' : '-');
+
+  if (result < (-2) ** 31 || result > 2 ** 31 - 1) return 0;
+
+  return result;
 }
