b45abe37bf6544b80150ff8124774becf8f2740e
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 02:53:27 +0200

    LC 006. Refactor to improve speed and overflows.

--- Exercises/007_reverse_integer.ts
+++ Exercises/007_reverse_integer.ts
@@ -1,11 +1,15 @@
 export default function reverse(x: number): number {
-  const result = +Math.abs(x)
-    .toString()
-    .split('')
-    .reverse()
-    .reduce((acc, n) => acc + n, x >= 0 ? '' : '-');
+  const reversed = [...Math.abs(x).toString()].reverse();
+  if (reversed.length > 10) return 0;
 
-  if (result < (-2) ** 31 || result > 2 ** 31 - 1) return 0;
+  if (reversed.length === 10 && +reversed[0] > 2) return 0;
 
-  return result;
+  if (reversed.length === 10 && +reversed[0] === 2) {
+    if (+reversed.slice(1, 10).join('') > (x >= 0 ? 147483646 : 147483647)) {
+      return 0;
+    }
+  }
+  return +reversed.reduce((acc, n) => acc.concat(n), x >= 0 ? '' : '-');
 }
+
+reverse(6463847422);
