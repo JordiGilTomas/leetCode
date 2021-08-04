97e90b4e8ab0b3f1192f26138c8af779df2a4354
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 12:13:08 +0200

    LC 008. Passed -+12 test.

--- Exercises/008_string_to_integer.ts
+++ Exercises/008_string_to_integer.ts
@@ -3,16 +3,19 @@ export default function myAtoi(s: string): number {
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] !== ' ') {
-      if (result.length && Number.isNaN(+s[i])) return +result;
-      if (!result.length && s[i] === '-') result = `${result}${s[i]}`;
-      else {
-        if (!result.length && Number.isNaN(+s[i])) {
-          return 0;
-        }
-        result = `${result}${s[i]}`;
+      if (result.length && Number.isNaN(+s[i])) {
+        return Number.isNaN(+result) ? 0 : +result;
       }
+
+      if (!result.length && s[i] === '-') result = `${result}${s[i]}`;
+      else if (!result.length && Number.isNaN(+s[i])) return 0;
+      else result = `${result}${s[i]}`;
     }
   }
 
-  return Math.max(Math.max((-2) ** 31, +result, Math.min(+result, 2 ** 31 - 1)));
+  return Math.max(
+    Math.max((-2) ** 31, +result, Math.min(+result, 2 ** 31 - 1)),
+  );
 }
+
+console.log(myAtoi('-+12'));
