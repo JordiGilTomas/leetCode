057036872a35ffbb5cb30da4436b890b5c000032
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 22:17:21 +0200

    LC 009. Improved speed and memory.

--- Exercises/009_palindrome_number.ts
+++ Exercises/009_palindrome_number.ts
@@ -2,14 +2,13 @@ export default function isPalindrome(x: number): boolean {
   if (x < 0) return false;
   if (x !== null && x >= 0 && x < 10) return true;
 
-  const digits = Math.floor(Math.log10(x));
-
-  for (let i = digits; i >= 0; i--) {
-    const actualDigit = Math.floor(x / 10 ** i) % 10;
-    const reversedDigit = Math.floor((x / 10 ** (digits - i)) % 10);
-
-    if (actualDigit !== reversedDigit) return false;
-    if (i === Math.floor(digits / 2)) return true;
+  for (let i = Math.floor(Math.log10(x)); i >= 0; i--) {
+    if (
+      Math.floor(x / 10 ** i) % 10 !==
+      Math.floor((x / 10 ** (Math.floor(Math.log10(x)) - i)) % 10)
+    )
+      return false;
+    if (i === Math.floor(Math.floor(Math.log10(x)) / 2)) return true;
   }
   return false;
 }
