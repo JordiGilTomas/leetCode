5e770df0c7a675d207d21e54a62b45dc4941bc24
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 18:29:00 +0200

    LC 009. Added initial tests.

---
+++ tests/009_palindrome_number.test.ts
@@ -0,0 +1,30 @@
+import isPalindrome from '../Exercises/009_palindrome_number';
+
+describe('Given an integer x, return true if x is palindrome integer', () => {
+  let x: number;
+  let expected: boolean;
+
+  it('121', () => {
+    x = 121;
+    expected = true;
+    expect(isPalindrome(x)).toBe(expected);
+  });
+
+  it('-121', () => {
+    x = -121;
+    expected = false;
+    expect(isPalindrome(x)).toBe(expected);
+  });
+
+  it('10', () => {
+    x = 10;
+    expected = false;
+    expect(isPalindrome(x)).toBe(expected);
+  });
+
+  it('-101', () => {
+    x = -101;
+    expected = false;
+    expect(isPalindrome(x)).toBe(expected);
+  });
+});
