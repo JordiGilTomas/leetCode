cef59bfab5478b8b140ed1f560547a1b940411d5
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 22:01:45 +0200

    LC 009. Passed all tests.

--- Exercises/009_palindrome_number.ts
+++ Exercises/009_palindrome_number.ts
@@ -2,10 +2,10 @@ export default function isPalindrome(x: number): boolean {
   if (x < 0) return false;
   if (x !== null && x >= 0 && x < 10) return true;
 
-  const digits = Math.round(Math.log10(x));
+  const digits = Math.floor(Math.log10(x));
 
-  for (let i = digits - 1; i >= 0; i--) {
-    const actualDigit = Math.trunc(x / 10 ** i) % 10;
+  for (let i = digits; i >= 0; i--) {
+    const actualDigit = Math.floor(x / 10 ** i) % 10;
     const reversedDigit = Math.floor((x / 10 ** (digits - i)) % 10);
 
     if (actualDigit !== reversedDigit) return false;
