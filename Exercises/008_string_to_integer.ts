4965fc65a1a9da2c47786d6ad4634baa959341c1
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-04 15:32:00 +0200

    LC 008. Passed '  +  413' test

--- Exercises/008_string_to_integer.ts
+++ Exercises/008_string_to_integer.ts
@@ -5,14 +5,26 @@ export default function myAtoi(s: string): number {
     if (Number.isInteger(+result) && result !== '' && s[i] === ' ') {
       return +result;
     }
+
     if (s[i] !== ' ') {
       if (result.length && Number.isNaN(+s[i])) {
         return Number.isNaN(+result) ? 0 : +result;
       }
 
-      if (!result.length && s[i] === '-') result = `${result}${s[i]}`;
-      else if (!result.length && Number.isNaN(+s[i]) && s[i] !== '+') return 0;
-      else result = `${result}${s[i]}`;
+      if (!result.length && s[i] === '-') {
+        result = `${result}${s[i]}`;
+      } else if (
+        !result.length &&
+        Number.isNaN(+s[i]) &&
+        s[i] !== '+' &&
+        s[i]
+      ) {
+        return 0;
+      } else {
+        result = `${result}${s[i]}`;
+      }
+    } else if (result.length) {
+      return 0;
     }
   }
 
