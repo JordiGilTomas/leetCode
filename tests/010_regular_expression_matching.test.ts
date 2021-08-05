65387d3b6879bda937416c2ed4d30208b5220c28
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-05 09:30:41 +0200

    LC 010. Added 'ab .*c' test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -54,4 +54,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = true;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('ab .*c', () => {
+    s = 'ab';
+    p = '.*c';
+
+    expected = false;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
