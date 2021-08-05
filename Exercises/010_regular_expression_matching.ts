41612ffb327d03a758e10b84b6a8f1e06ef81ec0
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-05 09:39:29 +0200

    LC 010. Passed 'ab .*c' test.

--- Exercises/010_regular_expression_matching.ts
+++ Exercises/010_regular_expression_matching.ts
@@ -6,32 +6,25 @@ export default function isMatch(s: string, p: string): boolean {
   let index = 0;
 
   for (let i = 0; i < p.length; i++) {
-    if (i === p.length - 1 && index === s.length) {
-      return false;
-    }
     if (p[i] === '*') continue;
     if (p[i + 1] === '*') {
-      if (p[i] === '.') return true;
+      if (i + 1 === p.length - 1) return true;
       while (p[i] === s[index]) {
         index += 1;
       }
-      if (index === s.length) return true;
       continue;
     }
     if (p[i] === '.') {
       index += 1;
+      if (index === s.length) return true;
       continue;
     }
 
     if (p[i] === s[index]) {
-      if (index + 1 === s.length) return true;
       index += 1;
+      if (index === s.length) return true;
       continue;
     }
-
-    if (p[i] !== s[index]) return false;
-
-    if (index === s.length) return true;
   }
   return false;
 }
