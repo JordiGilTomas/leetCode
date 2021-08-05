f9b6952f6dc1551ce9f0e17b4f9e09b3dae9a199
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-05 10:39:33 +0200

    LC 010. Passed 'a ab*a' test.

--- Exercises/010_regular_expression_matching.ts
+++ Exercises/010_regular_expression_matching.ts
@@ -11,7 +11,7 @@ export default function isMatch(s: string, p: string): boolean {
     if (p[i] === '*') continue;
     if (p[i + 1] === '*') {
       if (i + 1 === p.length - 1 && index + 1 === s.length - 1) return true;
-      while (p[i] === s[index] && index <= i + 1) {
+      while (p[i] === s[index] && index !== s.length - 1) {
         index += 1;
       }
       continue;
@@ -21,13 +21,12 @@ export default function isMatch(s: string, p: string): boolean {
       if (index === s.length) return true;
       continue;
     }
-
     if (p[i] === s[index]) {
       index += 1;
-      if (index === s.length) return true;
+      if (index === s.length && i === p.length - 1) return true;
       continue;
     }
-    if (index === s.length) return true;
+    if (index === s.length) return false;
   }
   return false;
 }
