a7ea0bd637edbda11ee140474d9e406c61cd8f0a
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-05 09:45:01 +0200

    LC 010. Passed 'abcd d*' test.

--- Exercises/010_regular_expression_matching.ts
+++ Exercises/010_regular_expression_matching.ts
@@ -8,7 +8,7 @@ export default function isMatch(s: string, p: string): boolean {
   for (let i = 0; i < p.length; i++) {
     if (p[i] === '*') continue;
     if (p[i + 1] === '*') {
-      if (i + 1 === p.length - 1) return true;
+      if (i + 1 === p.length - 1 && index + 1 === s.length - 1) return true;
       while (p[i] === s[index]) {
         index += 1;
       }
@@ -29,4 +29,4 @@ export default function isMatch(s: string, p: string): boolean {
   return false;
 }
 
-console.log(isMatch('ab', '.*'));
+console.log(isMatch('abcd', 'd*'));
