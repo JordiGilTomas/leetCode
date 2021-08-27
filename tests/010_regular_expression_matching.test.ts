be0a60a9730c2d20380092efd97e161a0d564d83
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-27 18:23:31 +0200

    LC 010. Added 'b ab*b' test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -166,4 +166,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = false;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('b ab*b', () => {
+    s = 'b';
+    p = 'ab*b';
+
+    expected = false;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
