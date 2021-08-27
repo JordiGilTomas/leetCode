ea74d84c96eed5f2bfce3382b1cc1033d35b8680
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-27 18:32:00 +0200

    LC 010. Added 'aba .*.*' test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -174,4 +174,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = false;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('aba .*.*', () => {
+    s = 'aba';
+    p = '.*.*';
+
+    expected = true;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
