b5ff80f5d6e2cbe64340744036ee68b417c97a8f
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-08-28 09:35:49 +0200

    LC 010. Added 'aabcbcbcaccbcaabc .*a*aa*.*b*.c*.*a*" test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -190,4 +190,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = false;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('aabcbcbcaccbcaabc .*a*aa*.*b*.c*.*a*', () => {
+    s = 'aabcbcbcaccbcaabc';
+    p = '.*a*aa*.*b*.c*.*a*';
+
+    expected = true;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
