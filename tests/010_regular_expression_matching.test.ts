7c0d70dfaacbd6e163830012b0b9522a3874a302
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-09-05 13:53:27 +0200

    LC 010. Added 'a .*..' test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -310,4 +310,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = true;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('a .*..', () => {
+    s = 'a';
+    p = '.*..';
+
+    expected = false;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
