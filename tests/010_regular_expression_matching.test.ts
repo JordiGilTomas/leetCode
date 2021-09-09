996000735745aaef148f6370c5cd8eb48bf59afe
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-09-09 20:21:16 +0200

    LC 010. Added 'bb ..*c' test.

--- tests/010_regular_expression_matching.test.ts
+++ tests/010_regular_expression_matching.test.ts
@@ -334,4 +334,12 @@ describe('Given an input string s and a pattern p, implement regular expression 
     expected = true;
     expect(isMatch(s, p)).toBe(expected);
   });
+
+  it('bb ..*c', () => {
+    s = 'bb';
+    p = '..*c';
+
+    expected = false;
+    expect(isMatch(s, p)).toBe(expected);
+  });
 });
