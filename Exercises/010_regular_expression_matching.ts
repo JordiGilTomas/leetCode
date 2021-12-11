46a645b6fa859fbbd7b045c9b44553bdc30d3d8f
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-12-11 21:38:34 +0100

    LC 010. Solution using dp bottom-up.

--- Exercises/010_regular_expression_matching.ts
+++ Exercises/010_regular_expression_matching.ts
@@ -1,15 +1,22 @@
 export default function isMatch(text: string, pattern: string): boolean {
-  if (pattern === "") return text === "";
+  const dp = Array(text.length + 1)
+    .fill(null)
+    .map(() => Array(pattern.length + 1).fill(false));
 
-  const firstMatch =
-    text !== "" &&
-    (pattern.charAt(0) === text.charAt(0) || pattern.charAt(0) === ".");
+  dp[text.length][pattern.length] = true;
 
-  if (pattern.length >= 2 && pattern.charAt(1) === "*") {
-    return (
-      isMatch(text, pattern.substring(2)) ||
-      (firstMatch && isMatch(text.substring(1), pattern))
-    );
+  for (let i = text.length; i >= 0; i--) {
+    for (let j = pattern.length - 1; j >= 0; j--) {
+      const firstMatch =
+        i < text.length &&
+        (pattern.charAt(j) === text.charAt(i) || pattern.charAt(j) === ".");
+
+      if (j + 1 < pattern.length && pattern.charAt(j + 1) === "*") {
+        dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+      } else {
+        dp[i][j] = firstMatch && dp[i + 1][j + 1];
+      }
+    }
   }
-  return firstMatch && isMatch(text.substring(1), pattern.substring(1));
+  return dp[0][0];
 }
