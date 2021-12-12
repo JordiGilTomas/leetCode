7ecdd9830567accf55e48be4d9ce114731bf32ab
JordiGilTomas <gil.tomas.jordi@gmail.com>
2021-12-12 16:12:38 +0100

    LC 011. Passed initial tests.

--- Exercises/011_container_with_most_water.ts
+++ Exercises/011_container_with_most_water.ts
@@ -1,3 +1,17 @@
 export default function maxArea(height: number[]): number {
-  return null;
+  let waterContainer = 0;
+
+  height.forEach((value, index) => {
+    let sum = 0;
+    for (let j = index + 1; j < height.length; j++) {
+      const actualIndex = j - index;
+      const actualValue = height[j];
+      if (actualValue <= value) {
+        sum = Math.max(sum, actualIndex * actualValue);
+      }
+    }
+    waterContainer = Math.max(waterContainer, sum);
+  });
+
+  return waterContainer;
 }
